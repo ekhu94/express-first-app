55672ca9fc23d4821d6038552c60bbe3073f52c7
Erik Huang <ekhu94@gmail.com>
2021-03-21 16:39:39 -0700

    practice using response.send

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(() => {
-    console.log("new request received");
+app.use((req, res) => {
+    console.log('new request received');
+    res.send({ name: 'axel', age: 2 })
 });
 
-app.listen(port, () => {
-    console.log(`Listening on Port ${port}`);
+app.listen(port, () =>{
+    console.log(`listening on port ${port}`);
 });
