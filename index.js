e0ff638779d769a22767296001d3f4b3e3ba9591
Erik Huang <ekhu94@gmail.com>
2021-03-21 16:54:05 -0700

    send responses for specific routes

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use((req, res) => {
-    console.log('new request received');
-    res.send({ name: 'axel', age: 2 })
+// app.use((req, res) => {
+//     console.log('new request received');
+//     res.send({ name: 'axel', age: 2 })
+// });
+
+app.get('/', (req, res) => {
+    res.send(`What do these animals say?`);
+});
+
+app.get('/dogs', (req, res) => {
+    res.send('Wan! Wan!');
+});
+
+app.get('/cats', (req, res) => {
+    res.send('Meooowwww');
+});
+
+app.get('/pigs', (req, res) => {
+    res.send('oink! oink!');
+});
+
+app.get('*', (req, res) => {
+    res.send('Could not find that page.');
 });
 
 app.listen(port, () =>{
