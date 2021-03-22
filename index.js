69e20c2a12602b6752e0f67cfd982c3b6a352cd7
Erik Huang <ekhu94@gmail.com>
2021-03-21 17:08:57 -0700

    send responses to path params

--- index.js
+++ index.js
@@ -8,20 +8,34 @@ const port = 3000;
 // });
 
 app.get('/', (req, res) => {
-    res.send(`What do these animals say?`);
+    res.send('<h1>Welcome to the home page</h1>');
 });
 
-app.get('/dogs', (req, res) => {
-    res.send('Wan! Wan!');
+app.get('/r/:subreddit', (req, res) => {
+    const {subreddit} = req.params;
+    res.send(`<h3>Browsing the ${subreddit} subreddit</h3>`);
 });
 
-app.get('/cats', (req, res) => {
-    res.send('Meooowwww');
+app.get('/r/:subreddit/comments/:postId', (req, res) => {
+    const {subreddit, postId} = req.params;
+    res.send(`<h4>Now viewing Comment id #${postId} in the ${subreddit} subreddit</h4>`);
 });
 
-app.get('/pigs', (req, res) => {
-    res.send('oink! oink!');
-});
+// app.get('/', (req, res) => {
+//     res.send(`What do these animals say?`);
+// });
+
+// app.get('/dogs', (req, res) => {
+//     res.send('Wan! Wan!');
+// });
+
+// app.get('/cats', (req, res) => {
+//     res.send('Meooowwww');
+// });
+
+// app.get('/pigs', (req, res) => {
+//     res.send('oink! oink!');
+// });
 
 app.get('*', (req, res) => {
     res.send('Could not find that page.');
